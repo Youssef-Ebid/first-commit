ec39ef200ef64cd41875b15c6a6f9c4b962d46b5
Youssef-Ebid <yousefebeid2016@gmail.com>
2024-12-02 14:05:26 +0200

    initial commit

---
+++ src/Friends.java
@@ -0,0 +1,16 @@
+class Friends extends User {
+    private boolean isFriend;
+
+    public Friends(String email, String password, String username, String gender, String birthDate, boolean isFriend) {
+        super(email, password, username, gender, birthDate);
+        this.isFriend = isFriend;
+    }
+
+    public boolean isFriend() {
+        return isFriend;
+    }
+
+    public void setFriend(boolean friend) {
+        isFriend = friend;
+    }
+}
